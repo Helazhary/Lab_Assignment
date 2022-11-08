c8a7557cf239fdd47235fb8dd2d9236316e8705f
Helazhary <98816029+Helazhary@users.noreply.github.com>
2022-11-08 16:53:17 +0200

    add zis
    zis to add

--- Untitle2/customer.cpp
+++ Untitle2/customer.cpp
@@ -7,7 +7,6 @@ customer::customer(QWidget *parent) :
 
 {
     ui->setupUi(this);
-    products = new product;
 }
 
 customer::~customer()
@@ -27,7 +26,40 @@ void customer::on_pushButton_5_clicked()
 {
 
 
-    ui->Name_List->setText(products->name);
+    for (int i=0; i< 10; i++) //10 is the preset size for the marketplace products array
+     if(ui->name->text()==cm.products[i].name)
+         ui->Name_List->setText("Product Name: "+cm.products[i].name+"\n "+ "Price: "+cm.products[i].price+"\n "+ "Quantity: "+cm.products[i].quantity+"\n "+ "Category: "+cm.products[i].category);
+
+}
+
+
+void customer::on_Scategory_clicked()
+{
+
+
+      for (int i=0; i< 10; i++)
+          if(ui->category->text()==cm.products[i].category)
+          {
+              ui->Category_List->setText(ui->Category_List->text()+"\n"+cm.products[i].name);
+          }
+
+}
+
+
+void customer::on_add_clicked()
+{
+
+    for (int i=0; i< 10; i++)
+     if((ui->product->text()==cm.products[i].name) && ui->quantity->text().toInt()>0 && ui->quantity->text()!="") //exception incase quantity exceeds available amount
+     {
+        ui->cart->setText(ui->cart->text()+"\n"+cm.products[i].name+"("+ui->quantity->text()+")"+"                                             "+cm.products[i].price);
+         total+=(cm.products[i].price.toDouble() * ui->quantity->text().toDouble());
+         ui->price->setText(QString::number(total));
+     }
+
+    ui->quantity->setText("");
+    ui->product->setText("");
+
 
 
 }
